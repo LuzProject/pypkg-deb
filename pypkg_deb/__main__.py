e60226d51fa07e8ce8ad79fbc83a49f229778b8d
ja1dan <ajaidan0@gmail.com>
2023-01-08 02:33:15 -0500

    feat: add pack command

--- pypkg_deb/__main__.py
+++ pypkg_deb/__main__.py
@@ -1,7 +1,7 @@
 # module imports
 from argparse import ArgumentParser
 from os import path
-from pydeb import Deb
+from pydeb import Deb, Pack
 
 # local imports
 from . import logger
@@ -10,7 +10,8 @@ from .utils import get_version
 def main(argv=None) -> None:
 	parser = ArgumentParser()
 	
-	parser.add_argument('deb', action='store', help='path of the deb file to operate on')
+	parser.add_argument('path', action='store', help='path to operate on')
+	parser.add_argument('-b', '--build', action='store_true', help='build a .deb file from a directory')
 	parser.add_argument('-x', '--extract', action='store_true', help='extracts the deb file')
 	parser.add_argument('-c', '--contents', action='store_true', help='list the contents of the deb file')
 	parser.add_argument('-v', '--version', action='version', version=f'pypkg-deb v{get_version()}',
@@ -18,24 +19,26 @@ def main(argv=None) -> None:
 	
 	args = parser.parse_args()
 	
-	if not path.exists(args.deb):
-		logger.error('Specified path to archive does not exist.')
+	if not path.exists(args.path):
+		logger.error('Specified path does not exist.')
 		exit(1)
 	
-	if not args.extract and not args.contents:
+	if not args.extract and not args.contents and not args.build:
 		logger.error('Please specify an operation.')
 		exit(1)
 	
-	save = (not args.extract)
-	
-	deb = Deb(file=args.deb, remove_file=save)
-	
 	if args.extract:
+		deb = Deb(file=args.path, remove_file=False)
 		logger.log(f'Successfully extracted "{deb.control.package}"')
 	if args.contents:
+		deb = Deb(file=args.path, remove_file=True)
 		logger.log('Contents:')
 		for f in deb.filepaths.get('root'):
 			print(f)
+	if args.build:
+		packed = Pack(args.path)
+		deb = Deb(file=packed.debpath, remove_file=True)
+		logger.log(f'Successfully built "{deb.control.package}"')
 	
 	
 if __name__ == '__main__':
